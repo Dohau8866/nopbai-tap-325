7d675dd855a2e891448db661649b1818784ced8c
Dohau8866 <dotat1110@gmail.com>
2025-04-04 22:32:33 +0700

    BAI TSAP 9
    Signed-off-by: Dohau8866 <dotat1110@gmail.com>

---
+++ document.js
@@ -0,0 +1,32 @@
+//document.ưhite("học javarclip co ban ");
+//khaibao ten day du cua 1 nguoi
+
+
+//var fullName = "Nguyen Van A";//string
+/* TỪ KHÓA VAR DÙNG ĐỂ KHAI BÁO BIẾN TRONG JAVASCRIPT
+let
+const
+var
+TÊn TIẾn 
+fullName : 
+          +dat threo quy tắc camelCase(con lac da )
+          +khong bat dau bang so 
+        + khong bat dau bbang ky tu dAC BIET 
+
+          Các kieu GIA TRI
+          +DON GAI(NUMBER.sTRING,BÔLEAN.,NUUL,UNDEFINED ,NAN)
+          +KIEU PHUC TAP
+          ARAY 
+          FUNCTION
+          OBJECT
+          
+          =+ phép gan gia tri
+          */
+         let age = 20;//number,string
+         //console.log("age",age);
+         if(true){
+             let age = 30;
+             console.log("age",age);
+         }
+         S
+
