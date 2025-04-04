0602014c1051aa35fe30d229213c41cffd7f5e3c
Dohau8866 <dotat1110@gmail.com>
2025-04-04 22:33:18 +0700

    BAI Tap 10
    Signed-off-by: Dohau8866 <dotat1110@gmail.com>

---
+++ bai1.js
@@ -0,0 +1,20 @@
+// main.js  
+
+// Sử dụng console.log để hiển thị "Hello World!" trên console của trình duyệt  
+console.log("Hello World!");  
+
+// Sử dụng document.write để hiển thị "Hello World!" lên trang web  
+document.write("Hello World!");  
+
+// Sử dụng alert để hiển thị "Hello World!" dưới dạng một thông báo  
+alert("Hello World!");  
+// main.js
+
+//sử dụng console.log để hiển thị "Hello World!" trên console của trình duyệt
+console.log("Hello World123!");
+
+// Sử dụng document.write để hiển thị "Hello World" lên trang web 
+document.write("Hello World!");
+
+// Sử dụng alert để hiển thị "Hello World" dưới dạng một thông báo 
+alert("Hello World!")
---
+++ bai2.js
@@ -0,0 +1,11 @@
+
+// Khai báo các biến 
+var i=10;       // Biến i kiểu số nguyên
+var f=20.5;     // Biến f kiểu số thực
+var b=true;     // Biến b kiểu logic
+var s="Hà Nội";// Biến s kiểu chuỗi
+
+document.write(i);
+document.write(f);
+document.write(b);
+document.write(s);
